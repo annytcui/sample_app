2c358eb4cbaca8208a79a346600059d043b28d72
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-13 16:12:42 +0800

    Fix logout bugs

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,4 +70,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "autenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
