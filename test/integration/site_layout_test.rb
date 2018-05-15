f32472f7cd92cbe7eba662d36b97724dd7169c50
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-15 14:10:11 +0800

    Add site layout integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:anny)
+  end
+
+  test "layout links for non-logged-in users" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -10,12 +14,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
-
-    get contact_path
-    assert_select "title", full_title("Contact")
-
-    get signup_path
-    assert_select "title", full_title("Sign up")
   end
 
+  test "layout links for logged-in users" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user), count: 2
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", signup_path, false
+  end
 end
