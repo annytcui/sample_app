01ed07df30d3fdbc462a16929d716622325416a1
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-15 10:41:06 +0800

    Friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,10 +17,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert session[:forwarding_url].nil?
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
