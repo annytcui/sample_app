f92878763dd4c44789a37a13192c94c218240dad
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-17 20:52:00 +0800

    Create Password Resets controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-
+  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
