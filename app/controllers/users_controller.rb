79685a8cd05d9850149a4fc5ac689cafb1e001d6
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-19 13:57:36 +0800

    Show microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated == true
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
