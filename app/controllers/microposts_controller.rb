209b77ece0d024ae3797552a3ffccd828374a856
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-19 17:48:41 +0800

    Add micropost create functionality

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,13 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(params[:content])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
