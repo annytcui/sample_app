c00a4203a6f92342c34413b6e55ee5d9e637d165
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-19 08:46:55 +0800

    Add User/Micropost associations

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,8 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:anny)
-    # This code is not idiomatically correct.
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
