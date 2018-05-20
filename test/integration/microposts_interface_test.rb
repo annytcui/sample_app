2514efd62ac68bdd078356b74dd5a6e52b597ffb
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-20 16:41:19 +0800

    Add image uploader test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     # Invalid submission
     assert_no_difference 'Micropost.count' do
@@ -19,9 +20,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/Pikachu.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
