05c8f69c3c01442e30f5c53a6600e4243e43a9f1
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-23 15:38:57 +0800

    Add validation test for Comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:anny)
+    @micropost = microposts(:ants)
+    @comment = Comment.new(content: "This is nonsense!",
+        micropost_id: @micropost.id, user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @comment.valid?
+  end
+
+  test "user id should be present" do
+    @comment.user_id = nil
+    assert_not @comment.valid?
+  end
+
+  test "micropost id should be present" do
+    @comment.micropost_id = nil
+    assert_not @comment.valid?
+  end
+
+  test "content should be present" do
+    @comment.content = "     "
+    assert_not @comment.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @comment.content = "a" * 141
+    assert_not @comment.valid?
+  end
 end
