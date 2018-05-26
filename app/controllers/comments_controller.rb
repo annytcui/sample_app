0bd63f93d4756904825732fa89c45e82dcb3db2f
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-26 18:22:49 +0800

    Implement access control to the comments destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: [:destroy]
 
   def create
     @micropost = Micropost.find(params[:micropost_id])
@@ -15,8 +16,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    micropost = Micropost.find(params[:micropost_id])
-    @comment = micropost.comments.find(params[:id])
     @comment.destroy
     flash[:success] = "Comment deleted"
     redirect_to request.referrer || root_url
@@ -26,5 +25,11 @@ class CommentsController < ApplicationController
 
     def comment_params
       params.require(:comment).permit(:content)
+    end
+
+    def correct_user
+      micropost = Micropost.find(params[:micropost_id])
+      @comment = micropost.comments.find(params[:id])
+      redirect_to request.referrer || root_url if @comment.nil?
     end
 end
