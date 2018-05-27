a2d27b3959f8dbc0817bd0f4d15de102a9831238
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-05-27 09:58:57 +0800

    Edit Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :micropost
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
   validates :micropost_id, presence: true
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 300 }
 end
